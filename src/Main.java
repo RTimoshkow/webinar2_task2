094dfb177def64919081787bf37f5e788902a0c5
Timoshkov Roman <timoshkow@mail.ru>
2021-10-15 10:09:53 +0300

    corrected formula

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100;
-        int transfer = 1500;
+        int transfer = 1869;
         int finalBalance;
 
         if (transfer > 1000) {
-            finalBalance = (int) (balance + transfer * 1.1);
+            finalBalance = (balance + transfer + transfer / 100);
         } else {
             finalBalance = balance + transfer;
         }
-       System.out.println("Ваш баланс = " + finalBalance);
+        System.out.println("Ваш баланс = " + finalBalance);
     }
 }
